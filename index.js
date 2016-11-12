4223d3f696592b364c65141018efa31ffc48ebf7
unknown <471060995@qq.com>
2016-11-12 21:08:29 +0800

    heroku

--- index.js
+++ index.js
@@ -7,19 +7,12 @@ var clientPath = path.join(__dirname, "./");
 var DOCUMENT_ROOT = path.join(__dirname, "./");
 
 
-// server.conf 功能
-// 支持 test/ 目录下面 .js js 脚本功能和 json 预览功能。
-// 注意这里面的.js，不是一般的.js 文件，而是相当于 express 的 route.
-app.use('/',require('yog-devtools')({
-    view_path: '',    // 避免报错。
-    rewrite_file: [path.join(DOCUMENT_ROOT, 'mock', 'server.conf')],
-    data_path: [path.join(DOCUMENT_ROOT, 'mock')]
-}));
-
 
 // 静态文件输出
 app.use('/',express.static(clientPath));
 
+
+app.use('/mock/area',require('./mock/area'))
 
 
 app.listen(app.get('port'), function () {
